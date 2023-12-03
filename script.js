39b36aca453a7a7f63a7e28ba42b1471b11ad2fb
GEDDA-star <manojgedda11@gmail.com>
2023-12-03 23:39:38 +0530

    changes moved to appropriate files

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
-var play=document.getElementById("play-state");
-var pause=document.getElementById("pause-state");
-var reset=document.getElementById("reset");
-var appendTens=document.getElementById("tens");
-var appendSeconds=document.getElementById("seconds");
-var tens= 0;
-var seconds= 0;
-var flg=false;
+var play = document.getElementById("play-state");
+var pause = document.getElementById("pause-state");
+var reset = document.getElementById("reset");
+var appendTens = document.getElementById("tens");
+var appendSeconds = document.getElementById("seconds");
+var tens = 0;
+var seconds = 0;
+var flg = false;
 var interval;
 
 console.log("working");
 function startTimer() {
     tens++;
-    
+
     if (tens < 9) {
-        console.log("tens"+tens);
+        console.log("tens" + tens);
         appendTens.innerHTML = "0" + tens;
         console.log(appendTens.innerHTML);
     }
@@ -33,44 +33,44 @@ function startTimer() {
     console.log(tens, seconds);
 }
 
-function showNotification(message){
+function showNotification(message) {
     var toastContainer = new bootstrap.Toast(document.getElementById('toast'));
     document.querySelector('.toast-body').innerText = message;
     toastContainer.show();
 }
 
 play.onclick = function () {
-    if(!flg){
-        flg=true;
+    if (!flg) {
+        flg = true;
         console.log("working");
-        play.style.display="none";
-        pause.style.display="inline";
-        
+        play.style.display = "none";
+        pause.style.display = "inline";
+
     }
     showNotification("Stop Watch Started!");
     console.log("timer started")
-if (interval) {
-    clearInterval(interval);
-}
-interval = setInterval(startTimer);
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(startTimer);
 };
 
 pause.onclick = function () {
-flg=false;
-pause.style.display="none";
-showNotification("Stop Watch Stopped!");
-play.style.display="inline";
+    flg = false;
+    pause.style.display = "none";
+    showNotification("Stop Watch Stopped!");
+    play.style.display = "inline";
 
-clearInterval(interval);
+    clearInterval(interval);
 };
 
 reset.onclick = function () {
-clearInterval(interval);
-showNotification("Reset!");
-tens = "00";
-seconds = "00";
-appendSeconds.innerHTML = seconds;
-appendTens.innerHTML = tens;
+    clearInterval(interval);
+    showNotification("Reset!");
+    tens = "00";
+    seconds = "00";
+    appendSeconds.innerHTML = seconds;
+    appendTens.innerHTML = tens;
 };
 
 
